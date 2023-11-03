a77282eb5e8609ae0fa2ce9d792b1c98cf570468
Vicktor007 <vicktord007@gmail.com>
2023-11-03 10:38:28 +0100

    auth pages layout completed

--- frontend001/src/pages/auth/ResetPassword.jsx
+++ frontend001/src/pages/auth/ResetPassword.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import styles from "./auth.module.scss";
+import { MdPassword } from "react-icons/md";
+import Card from "../../components/card/Card"
+import { Link } from 'react-router-dom';
 
 const ResetPassword = () => {
   return (
-    <div>ResetPassword</div>
+    <div className={`container ${styles.auth}`}>
+        <Card>
+            <div className={styles.form}>
+                <div className='--flex-center'>
+                    <MdPassword size={35} color='#999'/>
+                </div>
+                <h2>Reset Password</h2>
+                <form>
+                    <input type='password'
+                    placeholder='New Password'
+                    required name='password'/>
+
+                    <input type='password'
+                    placeholder='Confirm New Password'
+                    required name='password'/>
+                    
+                    <button type='submit' className='--btn --btn-primary --btn-block'>
+                       Reset Password
+                    </button>
+                    <div className={styles.links}>
+                    <p>
+                    <Link to="/">- Home</Link>
+
+                    </p>
+                    <p>
+
+                    <Link to="/register">- Login</Link>
+                    </p>
+                </div>
+                </form>
+               
+                
+            </div>
+        </Card>
+    </div>
   )
 }
 
