d957f6c624ae5cd3511974664de4a085a7abce8d
Vicktor007 <vicktord007@gmail.com>
2023-11-04 09:11:08 +0100

    user Authentication complete

--- frontend001/src/App.jsx
+++ frontend001/src/App.jsx
@@ -6,13 +6,18 @@ import ResetPassword from "./pages/auth/ResetPassword";
 import Register from "./pages/auth/Register";
 import Sidebar from "./components/sidebar/Sidebar";
 import Dashboard from "./pages/dashboard/Dashboard";
-import Layout from "./components/layout/Layout"
+import Layout from "./components/layout/Layout";
+import axios from "axios";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+axios.defaults.withCredentials = true; //axios feature for saving user credentials to backend db
 
 
 function App() {
   return (
     <BrowserRouter>
+    <ToastContainer />
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/register" element={<Register/>}/>
--- frontend001/src/components/header/Header.jsx
+++ frontend001/src/components/header/Header.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { selectName, SET_LOGIN } from "../../redux/features/auth/authSlice";
+import { logoutUser } from "../../services/authService";
+
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const name = useSelector(selectName);
+
+
+  const logout = async () => {
+    await logoutUser();
+    await dispatch(SET_LOGIN(false));
+    navigate("/login");
+  }; 
+
   return (
     <div className='--pad header'>
         <div className='--flex-between'>
@@ -8,10 +24,10 @@ const Header = () => {
                 <span className="--fw-thin">
                     welcome, </span>
                 <span className="--color-danger">
-                     Victor
+                    {name}
                 </span>
             </h3>
-            <button className='--btn --btn-danger'>Logout</button>
+            <button onClick={logout} className='--btn --btn-danger'>Logout</button>
         </div>
         <hr />
     </div>
