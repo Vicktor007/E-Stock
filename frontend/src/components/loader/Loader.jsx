7ee49af2e823a27f52d74762a1b8dfa159f4e824
Vicktor007 <vicktord007@gmail.com>
2023-11-09 20:44:20 +0100

    spinner image modified

--- frontend/src/components/loader/Loader.jsx
+++ frontend/src/components/loader/Loader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import loaderImg from "../../assets/loader.gif";
+import loaderImg from "../../assets/loader2.gif";
 import ReactDOM from "react-dom";
 import "./Loader.scss";
 
@@ -7,7 +7,8 @@ const Loader = () => {
   return ReactDOM.createPortal(
     <div className="wrapper">
       <div className="loader">
-        <img src={loaderImg} alt="Loading..." />
+      <img src={loaderImg} alt="Loading..." style={{ width: '100px' }} />
+
       </div>
     </div>,
     document.getElementById("loader")
@@ -17,7 +18,8 @@ const Loader = () => {
 export const SpinnerImg = () => {
   return (
     <div className="--center-all">
-      <img src={loaderImg} alt="Loading..." />
+      <img src={loaderImg} alt="Loading..." style={{ width: '100px' }} />
+
     </div>
   );
 };
